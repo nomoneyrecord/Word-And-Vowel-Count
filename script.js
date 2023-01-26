c4e8c6b65b138e823a076951d11f9ac146b4eafc
Gary H <3rdeye.ghj@gmail.com>
2023-01-26 15:08:14 -0800

    Worked through the word counting logic, but it's faulty
    logic is counting spaces and therefor not counting the words accurately

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function countWords(){
+  const words = document.getElementById("input").value;
+  let numWord = 0; 
+  const split = words.split(' ');
 
+  for (let i = 0; i < words.length; i++) {
+    const currentCharacter = words[i];
+    if (currentCharacter == " ") {
+      numWord += 1;
+    }
+  }
+  numWord += 1;
+
+  document.getElementById("show").innerText = numWord;
+}
