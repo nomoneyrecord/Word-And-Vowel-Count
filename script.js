606e9935c53001db48b253f0db462cf59bcc1875
Gary H <3rdeye.ghj@gmail.com>
2023-01-31 12:21:07 -0800

    Trying to add count vowel function but it stops my word count function from working so trying to figure that out

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function countWords(){
   let words = document.getElementById("input").value;
   let numWord = 0; 
-  let split = words.split(' ');
+  let split = words.split(" ");
 
   for (let i = 0; i < split.length; i++) {
     const currentCharacter = words[i];
@@ -12,17 +12,7 @@ function countWords(){
 
   document.getElementById("show").innerText = numWord;
 }
-const vowels = ["a", "e", "i", "o", "u"]
 
-function countVowels(text) {
-  let count = 0;
-  let countVowels = document.getElementById("input").value;
+//const vowels = ['a', 'e', 'i', 'o', 'u']
 
-for (let letter of text.toLowerCase()) {
-  if (countVowels.includes(letter)) {
-    count++
-  }
-}
-
-
-}
+//function countVowels()
